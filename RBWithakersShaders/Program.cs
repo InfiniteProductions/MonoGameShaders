63ce86db4147412256d71b541d7fac73f8c1f726
InfiniteProductions <justice8@bbox.fr>
2016-04-09 15:37:25 +0200

    Add a first batch of RBW tutorials
    Model converted to new FBX version + ambient, diffuse and specular
    shaders

---
+++ RBWithakersShaders/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace RBWithakersShaders
+{
+#if WINDOWS || LINUX
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Specular())
+                game.Run();
+        }
+    }
+#endif
+}
---
+++ RBWithakersShaders/Specular.cs
@@ -0,0 +1,119 @@
+﻿using System;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace RBWithakersShaders
+{
+    public class Specular : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Effect effect;
+
+        Matrix world = Matrix.CreateTranslation(0, 0, 0);
+        Matrix view = Matrix.CreateLookAt(new Vector3(0, 0, 10), new Vector3(0, 0, 0), new Vector3(0, 1, 0));
+        Matrix projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(45), 800f / 600f, 0.1f, 100f);
+        float angle = 0;
+        float distance = 10;
+
+        Model model;
+
+        Vector3 viewVector;
+
+        public Specular()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            model = Content.Load<Model>("Models/Helicopter");
+            //effect = Content.Load<Effect>("Effects/Ambient");
+            //effect = Content.Load<Effect>("Effects/Diffuse");
+            effect = Content.Load<Effect>("Effects/Specular");
+        }
+
+        protected override void UnloadContent()
+        {
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            angle += 0.01f;
+            //view = Matrix.CreateLookAt(distance * new Vector3((float)Math.Sin(angle), 0, (float)Math.Cos(angle)), new Vector3(0, 0, 0), new Vector3(0, 1, 0));
+
+            //specular
+            Vector3 cameraLocation = distance * new Vector3((float)Math.Sin(angle), 0, (float)Math.Cos(angle));
+            Vector3 cameraTarget = new Vector3(0, 0, 0);
+            viewVector = Vector3.Transform(cameraTarget - cameraLocation, Matrix.CreateRotationY(0));
+            viewVector.Normalize();
+            view = Matrix.CreateLookAt(cameraLocation, cameraTarget, new Vector3(0, 1, 0));
+
+            base.Update(gameTime);
+        }
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            //DrawModel(model, world, view, projection);
+
+            DrawModelWithEffect(model, world, view, projection);
+            base.Draw(gameTime);
+        }
+
+        private void DrawModel(Model model, Matrix world, Matrix view, Matrix projection)
+        {
+            foreach (ModelMesh mesh in model.Meshes)
+            {
+                foreach (BasicEffect effect in mesh.Effects)
+                {
+                    effect.EnableDefaultLighting();
+                    effect.PreferPerPixelLighting = true;
+                    effect.World = world * mesh.ParentBone.Transform;
+                    effect.View = view;
+                    effect.Projection = projection;
+                }
+                mesh.Draw();
+            }
+        }
+
+        private void DrawModelWithEffect(Model model, Matrix world, Matrix view, Matrix projection)
+        {
+            foreach (ModelMesh mesh in model.Meshes)
+            {
+                foreach (ModelMeshPart part in mesh.MeshParts)
+                {
+                    part.Effect = effect;
+                    effect.Parameters["World"].SetValue(world * mesh.ParentBone.Transform);
+                    effect.Parameters["View"].SetValue(view);
+                    effect.Parameters["Projection"].SetValue(projection);
+
+                    //effect.Parameters["AmbientColor"].SetValue(Color.Green.ToVector4());
+                    //effect.Parameters["AmbientIntensity"].SetValue(0.5f);
+
+                    //diffuse
+                    Matrix worldInverseTransposeMatrix = Matrix.Transpose(Matrix.Invert(mesh.ParentBone.Transform * world));
+                    effect.Parameters["WorldInverseTranspose"].SetValue(worldInverseTransposeMatrix);
+
+                    //specular
+                    effect.Parameters["ViewVector"].SetValue(viewVector);
+                }
+                mesh.Draw();
+            }
+        }
+    }
+}
